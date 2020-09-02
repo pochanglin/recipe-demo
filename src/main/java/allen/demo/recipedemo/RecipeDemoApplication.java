e0687af70c723068e69278a48540978d84598fb3
PoChangLin <allen4353968@gmail.com>
2020-09-02 18:16:00 +0800

    First commit

---
+++ src/main/java/allen/demo/recipedemo/RecipeDemoApplication.java
@@ -0,0 +1,13 @@
+package allen.demo.recipedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/allen/demo/recipedemo/RecipeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package allen.demo.recipedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
