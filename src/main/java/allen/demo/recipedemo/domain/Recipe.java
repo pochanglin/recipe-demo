49c902ad9ac4c154dbf52d2b5d0eeefc4d2ae013
PoChangLin <allen4353968@gmail.com>
2020-12-17 23:56:04 +0800

    adding mant to many category and h2 property

--- src/main/java/allen/demo/recipedemo/domain/Recipe.java
+++ src/main/java/allen/demo/recipedemo/domain/Recipe.java
@@ -24,6 +24,14 @@ public class Recipe {
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
+    @ManyToMany
+    @JoinTable(
+            name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id")
+    )
+    private Set<Category> categories;
+
     public Long getId() {
         return id;
     }
@@ -118,5 +126,13 @@ public class Recipe {
 
     public void setDifficulty(Difficulty difficulty) {
         this.difficulty = difficulty;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
